e9a0c39f59e5daac14aee5c169b027074aef15ab
remi0628 <safaia0628@yahoo.co.jp>
2020-07-12 18:57:46 +0900

    fix

--- crawler_main.py
+++ crawler_main.py
@@ -13,13 +13,8 @@ BLANK_URL = 'https://www.nankankeiba.com/uma_info/2017100322.do'
 
 
 def main():
-    race_top, condition, race_len, race_date = result_data(URL) # レース当日データ取得
-    print('#レース当日データ#\n', '日付：{}, レース距離：{}, 土の状態：{}, 1位馬番：{}'.format(race_date, race_len, condition, race_date))
-    blank_link_list = horse_page_link(URL)
-    for i in range(len(blank_link_list)):
-        print(blank_link_list[i])
-    soup = url_to_soup(BLANK_URL)
-    print(horse_data(BLANK_URL))
+    create_data_frame(URL)
+    #print(horse_data(BLANK_URL))
 
 
 def url_to_soup(url): # レース情報ページ取得
@@ -42,8 +37,8 @@ def get_previous_race_row(soup): # 競走馬詳細データから出走履歴取
 def horse_data(url): # 出走履歴からデータ作成
     soup = url_to_soup(url)
     blank_race_data = get_previous_race_row(soup) # 過去のレースデータ
-    print('出走履歴1番目の日付：', re.split('[<>]' ,blank_race_data[1:2][0][2])[2])
-    print(len(blank_race_data))
+    # print('出走履歴1番目の日付：', re.split('[<>]' ,blank_race_data[1:2][0][2])[2])
+    # print(len(blank_race_data))
     #blank_race_day_calc(blank_race_data)
     #print(re.split('[<>]' ,blank_race_data))
     df =  pd.DataFrame(blank_race_data)[1:][[2,3,10,11,13,14,15,19,23]].dropna().rename(columns={
@@ -75,6 +70,14 @@ def race_day(soup): # レース日 datetimeオブジェクトに変換 return da
     race_day = datetime.date(year=int(today[0]), month=int(today[1]), day=int(today[2]))
     return race_day
 
+
+def create_data_frame(url): #データフレーム作成
+    race_top, condition, race_len, race_date = result_data(URL) # レース当日データ取得
+    print('#レース当日データ#\n', '日付：{}, レース距離：{}, 土の状態：{}, 1位馬番：{}'.format(race_date, race_len, condition, race_date))
+    blank_link_list = horse_page_link(URL)
+    for i in range(len(blank_link_list)):
+        print(horse_data(blank_link_list[i]))
+    soup = url_to_soup(BLANK_URL)
 
 if __name__ == '__main__':
     main()

65cd2f56b88a3084cf257dd24d751b2ab1376948
remi0628 <safaia0628@yahoo.co.jp>
2020-07-16 07:15:07 +0900

    crawler fix. All data csv file can be created.

--- crawler_csv.py
+++ crawler_csv.py
@@ -1,62 +1,67 @@
-import csv
+import os
+
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import pandas as pd
+import numpy as np
 
 
-def horse_data_csv(url, date=datetime.date.today(), folder=""):
+def horse_data_csv(url, date=datetime.date.today(), number=0, dir=None):
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
 
     horse_name = soup.find('h2', id="tl-prof").text  # 馬の名称
     horse_birth = soup.find_all('td', class_="w30pr")[0].text  # 馬の誕生日
 
-    csv_name = folder + horse_birth + horse_name + date.strftime('%Y-%m-%d') + ".csv"
-
     # 競走馬詳細データサイト内には表が3つ　その内3つめの出走履歴を取得
     tab = soup.find_all('table', class_='tb01 w100pr bg-over stripe al-center')[2]
-    with open(csv_name, "w", newline="", encoding='SHIFT-JIS') as file:
-        writer = csv.writer(file)
-        rows = tab.find_all("tr")
-        for i, row in enumerate(rows):
-            if i == 0:
-                writer.writerow(['年月日', '競馬場', 'R', 'レース名', '距離', '天候馬場', '馬番', '人気',
-                                 '着順', 'タイム', '差/事故', '上3F', 'コーナー通過順', '体重', '騎手',
-                                 '負担重量', '調教師', '獲得賞金（円）', 'レース名url', '騎手url', '調教師url'])
-                continue
+    pdList = []
+    rows = tab.find_all("tr")
+    for i, row in enumerate(rows):
+        if i == 0:
+            continue
 
-            csvRow = []
-            urls = []
-            for j, cell in enumerate(row.findAll(['td', 'th'])):
-                if j == 0:
-                    y, m, d = [int(x) for x in cell.get_text().split("/")]
-                    if y < 50:
-                        y += 2000
-                    else:
-                        y += 1900
-                    if datetime.date(y, m, d) >= date:
-                        break
-                if j == 3 or j == 14 or j == 16:
+        csvRow = []
+        urls = []
+        for j, cell in enumerate(row.findAll(['td', 'th'])):
+            if j == 0:
+                y, m, d = [int(x) for x in cell.get_text().split("/")]
+                if y < 50:
+                    y += 2000
+                else:
+                    y += 1900
+                if datetime.date(y, m, d) >= date:
+                    break
+            if j == 3 or j == 14 or j == 16:
+                try:
                     urls.append(cell.find("a").get('href'))
+                except AttributeError:
+                    urls.append(0)
 
-                csvRow.append(cell.get_text().replace('\n', ''))
+            csvRow.append(cell.get_text().replace('\n', ''))
 
-            if csvRow:
-                csvRow += urls
-                writer.writerow(csvRow)
+        if csvRow:
+            csvRow += urls
+            pdList.append(csvRow)
 
-    return csv_name
+    df = pd.DataFrame(pdList, columns=['年月日', '競馬場', 'R', 'レース名', '距離', '天候馬場', '馬番', '人気',
+                                       '着順', 'タイム', '差/事故', '上3F', 'コーナー通過順', '体重', '騎手',
+                                       '負担重量', '調教師', '獲得賞金（円）', 'レース名url', '騎手url', '調教師url'])
+
+    if dir:
+        os.makedirs(dir, exist_ok=True)
+        number=str(number+1)
+        df.to_csv(dir + number + horse_name + horse_birth + ".csv", encoding="SHIFT-JIS")
+
+    return df
 
 
 def main():
     BLANK_URL = 'https://www.nankankeiba.com/uma_info/2017102763.do'
 
-    day = datetime.date(2019, 12, 9)
-    horse_path = horse_data_csv(BLANK_URL, day, "data/")
-    print(horse_path)
-
-    import pandas as pd
-    df = pd.read_csv(horse_path)
-    print(df)
+    day = datetime.date(2019, 11, 9)
+    df, _, _ = horse_data_csv(BLANK_URL, day)
+    print(df[:])
 
 
 if __name__ == '__main__':
--- crawler_main.py
+++ crawler_main.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup as bs4
 from urllib.request import urlopen, urljoin, urlparse
 
 import crawler_settings  # crawlerの設定ファイル
-from crawler_csv import horse_data_csv # 馬の詳細URL渡してcsvファイル作成
-from race_link_collection import horse_race_list # 半期開催日程URLを渡すと半期分のレースURLを返す
+from crawler_csv import horse_data_csv  # 馬の詳細URL渡してcsvファイル作成
+from race_link_collection import horse_race_list  # 半期開催日程URLを渡すと半期分のレースURLを返す
 
 CSV_DATA_PATH = crawler_settings.CSV_DATA_PATH
 HOME_URL = crawler_settings.HOME_URL
@@ -18,22 +18,27 @@ BLANK_URL = 'https://www.nankankeiba.com/uma_info/2017100322.do'
 RACE_LIST_HELF_PERIOD = 'https://www.nankankeiba.com/calendar/202004.do'
 
 
-def url_to_soup(url): # レース情報ページ取得
+def url_to_soup(url):  # レース情報ページ取得
     req = requests.get(url)
-    return bs4(req.content, 'html.parser') # 'lmxl'より処理が速い
+    return bs4(req.content, 'html.parser')  # 'lmxl'より処理が速い
 
-def horse_page_link(url): # 各馬の過去情報URLリスト取得
+
+def horse_page_link(url):  # 各馬の過去情報URLリスト取得
     soup = url_to_soup(url)
-    link_list = [HOME_URL + x.get('href') for x in soup.find_all('a', class_='tx-mid tx-low') ] # サイト内リンクから過去情報リンクのみ取得
+    link_list = [HOME_URL + x.get('href') for x in soup.find_all('a', class_='tx-mid tx-low')]  # サイト内リンクから過去情報リンクのみ取得
     return link_list
+
 
 # lamda
 tag_to_text = lambda x: x.split('\n')
 split_tr = lambda x: str(x).split('</tr>')
 
-def get_previous_race_row(soup): # 競走馬詳細データから出走履歴取得
-    race_table = soup.find_all('table', class_='tb01 w100pr bg-over stripe al-center')[2] # 競走馬詳細データサイト内には表が3つ　その内3つめの出走履歴を取得
+
+def get_previous_race_row(soup):  # 競走馬詳細データから出走履歴取得
+    race_table = soup.find_all('table', class_='tb01 w100pr bg-over stripe al-center')[
+        2]  # 競走馬詳細データサイト内には表が3つ　その内3つめの出走履歴を取得
     return [tag_to_text(x) for x in split_tr(race_table)]
+
 
 '''
 def horse_data(url): # 出走履歴からデータ作成
@@ -56,19 +61,21 @@ def blank_race_day_calc(blank_race_data): # 出走履歴何番目を取得する
         print(race_day)
 '''
 
+
 # 当日データ取得
-def result_data(url): # レース結果取得 return[1着馬, 土の状態, レースの長さ, レース日]
+def result_data(url):  # レース結果取得 return[1着馬, 土の状態, レースの長さ, レース日]
     result_url = url[0:28] + 'result' + url[-20:]
     print(result_url)
     soup = url_to_soup(result_url)
-    condition = soup.find(id="race-data02").get_text().replace('\n','').split('　')[2][0:1] # 土の状態
-    race_len = int(soup.find(id="race-data01-a").get_text().replace('\n','').split('　')[3].replace(',','')[1:5]) # レースの長さ
-    race_top = soup.find('tr', class_='bg-1chaku').contents[5].string # 1位
-    race_date = race_day(soup) # レース日付
+    condition = soup.find(id="race-data02").get_text().replace('\n', '').split('　')[2][0:1]  # 土の状態
+    race_len = int(soup.find(id="race-data01-a").get_text().replace('\n', '').split('　')[3].replace(',', '')[1:5].replace("m",""))  # レースの長さ
+    race_top = soup.find('tr', class_='bg-1chaku').contents[5].string  # 1位
+    race_date = race_day(soup)  # レース日付
     return race_top, condition, race_len, race_date
 
+
 # レース日取得
-def race_day(soup): # レース日 datetimeオブジェクトに変換 return datetime.date[y-m-d]
+def race_day(soup):  # レース日 datetimeオブジェクトに変換 return datetime.date[y-m-d]
     today = soup.find('span', class_='tx-small').text.strip()
     today = re.split('[年月日]', today)
     del today[-1]
@@ -76,29 +83,28 @@ def race_day(soup): # レース日 datetimeオブジェクトに変換 return da
     return race_day
 
 
-def create_data_frame(url): #データフレーム作成
-    df = []
-    race_top, condition, race_len, race_date = result_data(url) # レース当日データ取得
+def create_data_frame(url):  # データフレーム作成
+    try:
+        race_top, condition, race_len, race_date = result_data(url)  # レース当日データ取得
+        # race_top, condition, race_len, race_date, race_number = result_data(url)  理想
+    except:
+        return 1
     print('#レース当日データ#\n', '日付：{}, レース距離：{}, 土の状態：{}, 1位馬番：{}'.format(race_date, race_len, condition, race_top))
     blank_link_list = horse_page_link(url)
     for i in range(len(blank_link_list)):
-        horse_path = horse_data_csv(blank_link_list[i], race_date, CSV_DATA_PATH)
-        #df.append(pd.read_csv(horse_path, encoding='SHIFT-JIS')) # テスト時はコメントアウト
-    return horse_path
+        horse_data_csv(blank_link_list[i], race_date, i, f"data/race/{race_date}-{race_len}-{condition}-{race_top}/")
+        # horse_data_csv(blank_link_list[i], race_date, i, f"data/race/{race_date}-{race_number}-{race_len}-{condition}-{race_top}/")  理想
 
 
-def create_data_csv(url): # 半期分レースに出場した馬のCSVファイル作成
-    horse_path_list = []
+def create_data(url):  # 半期分レースに出場した馬のCSVファイル作成
     helf_piriod_race_list = horse_race_list(url)
-    for i in range(len(helf_piriod_race_list)): # 半期全レースページを渡す
-        horse_path_list.append(create_data_frame(helf_piriod_race_list[i]))
-    print('馬のデータを{}個CSVデータにしました。'.format(len(list(itertools.chain.from_iterable(horse_path_list)))))
-    return list(itertools.chain.from_iterable(horse_path_list)) # 2次元リストを1次元リストに平坦化
+    for i in range(len(helf_piriod_race_list)):  # 半期全レースページを渡す
+        create_data_frame(helf_piriod_race_list[i])
+    print('レースデータを{}個保存しました。'.format(len(helf_piriod_race_list)))
 
 
 def main():
-    create_data_csv(RACE_LIST_HELF_PERIOD)
-
+    create_data(RACE_LIST_HELF_PERIOD)
 
 
 if __name__ == '__main__':
